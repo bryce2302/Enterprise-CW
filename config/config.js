c896f8931178b9394903c1602328e4ce461b11e5
BRYCE DAWSON (1801711) <b.dawson1@rgu.ac.uk>
2022-03-22 15:56:44 +0000

    Changed Mongo uri

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
 
-mongoUri: "mongodb://bryce_2302:Password12@cluster0.9ocoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" 
+mongoUri: "mongodb+srv://bryce_2302:Password12@cluster0.9ocoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" 
 
 }
 
