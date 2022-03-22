2b4d2b25afe16740ebe9a9bc31612e21b3b9b91d
BRYCE DAWSON (1801711) <b.dawson1@rgu.ac.uk>
2022-03-22 16:44:10 +0000

    Updated to local and onlin e working

--- config/config.js
+++ config/config.js
@@ -3,7 +3,10 @@ const config = {
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
 
-mongoUri: "mongodb+srv://bryce_2302:Password12@cluster0.9ocoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" 
+
+
+mongoUri: process.env.MONGOURI || 'mongodb://' + (process.env.IP || 'localhost') + ':' + 
+(process.env.MONGO_PORT || '27017') + '/mydb/'
 
 }
 
