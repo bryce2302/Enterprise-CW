217fd87f8d06f215655b7bfdbb6808b302dd16aa
BRYCE DAWSON (1801711) <b.dawson1@rgu.ac.uk>
2022-03-22 16:56:19 +0000

    Updated homePage

--- client/core/Home.js
+++ client/core/Home.js
@@ -77,7 +77,7 @@ export default function Home(){
           <Typography variant="body2" component="p" className={classes.credit} color="textSecondary">Photo: Chompster</Typography>
           <CardContent>
             <Typography variant="body1" component="p">
-              Welcome to Lab 6 home page.
+              Welcome to my CW home page.
               <div>
               {jokes.joke}
               </div>
