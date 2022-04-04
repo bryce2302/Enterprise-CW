4309c4dabc67352c273119e34861c08977f4ba46
bryce2302 <bryce_dawson@outlook.com>
2022-04-04 15:01:06 +0100

    changed router

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -23,7 +23,7 @@ const MainRouter = () => {
         <PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
         <Route path="/user/:userId" component={Profile}/>
         <Route path="/useradmin/:userId" component={UserAdmin}/>
-        <Route path="/app" component={App}/>
+        <Route path="/Shop/app" component={App}/>
       </Switch>
     </div>)
 }
