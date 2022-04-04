530199f25a1eaa3e8a4237bdc5e5f94601e8e44d
bryce2302 <bryce_dawson@outlook.com>
2022-04-04 13:17:27 +0100

    color

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -10,7 +10,7 @@ import {Link, withRouter} from 'react-router-dom'
 
 const isActive = (history, path) => {
   if (history.location.pathname == path)
-    return {color: '#4937d4'}
+    return {color: '#2ac9b4'}
   else
     return {color: '#ffffff'}
 }
