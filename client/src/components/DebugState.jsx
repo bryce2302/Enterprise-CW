a21c0f15e58c7730b44789e7792d674ac7bf26f9
bryce2302 <43371125+bryce2302@users.noreply.github.com>
2022-04-05 15:19:46 +0100

    deleting broken game

--- client/src/components/DebugState.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { puke } from 'helpers/utils';
-
-const styles = { position: 'fixed', top: 0, left: 16 };
-export default ({ data }) => <div style={styles}>{puke(data)}</div>
