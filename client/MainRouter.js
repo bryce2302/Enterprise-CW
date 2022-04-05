b51e77e2d1e55ab30a6f4ed8366685ccc35f8be1
bryce2302 <43371125+bryce2302@users.noreply.github.com>
2022-04-05 15:03:48 +0100

    full route

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -9,7 +9,8 @@ import PrivateRoute from './auth/PrivateRoute'
 import Profile from './user/Profile'
 import EditProfile from './user/EditProfile'
 import UserAdmin from './user/UsersAdmin'
-import Game from 'src/containers/Game'
+import Game from 'c:/Users/Username/Enterprise-CW/Enterprise-CW/src/component/Game'
+
 
 
 const MainRouter = () => {
