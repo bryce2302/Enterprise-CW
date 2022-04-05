0c51b01a781ac753534d3650cc97df703d7017f4
bryce2302 <43371125+bryce2302@users.noreply.github.com>
2022-04-05 14:59:15 +0100

    new game

--- client/user/UsersAdmin.js
+++ client/user/UsersAdmin.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import List from '@material-ui/core/List'
@@ -11,11 +11,12 @@ import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
 import ArrowForward from '@material-ui/icons/ArrowForward'
 import Person from '@material-ui/icons/Person'
-import {Link} from 'react-router-dom'
-import {listadmin} from './api-user.js'
+import { Link } from 'react-router-dom'
+import { listadmin } from './api-user.js'
 import auth from './../auth/auth-helper'
 import DeleteUser from './DeleteUser.js'
-import {read} from './api-user.js'
+import { read } from './api-user.js'
+
 
 const useStyles = makeStyles(theme => ({
   root: theme.mixins.gutters({
@@ -37,50 +38,50 @@ export default function Users({ match }) {
     const abortController = new AbortController()
     const signal = abortController.signal
 
-    listadmin({userId: match.params.userId}, {t: jwt.token}, signal).then((data) => {
+    listadmin({ userId: match.params.userId }, { t: jwt.token }, signal).then((data) => {
       if (data && data.error) {
         console.log(data.error)
       } else {
-      	console.log("Here is the user data")
-      	console.log(data)
+        console.log("Here is the user data")
+        console.log(data)
         setUsers(data)
       }
     })
 
-    return function cleanup(){
+    return function cleanup() {
       abortController.abort()
     }
   }, [match.params.userId])
 
 
-    return (
-      <Paper className={classes.root} elevation={4}>
-        <Typography variant="h6" className={classes.title}>
-          All Users ({users.length})
-        </Typography>
-        <List dense>
-         {users.map((item, i) => {
+  return (
+    <Paper className={classes.root} elevation={4}>
+      <Typography variant="h6" className={classes.title}>
+        All Users ({users.length})
+      </Typography>
+      <List dense>
+        {users.map((item, i) => {
           return <Link to={"/user/" + item._id} key={i}>
-                    <ListItem button>
-                      <ListItemAvatar>
-                        <Avatar>
-                          <Person/>
-                        </Avatar>
-                      </ListItemAvatar>
-                      <ListItemText primary={item.name}/>
-                      <ListItemText primary={"About: " + item.about}/>
-                      <ListItemText primary={"Administrator: " + item.admin}/>
-                      <DeleteUser userId={item._id}/>
-                      <ListItemSecondaryAction>
-                      <IconButton>
-                          <ArrowForward/>
-                      </IconButton>
-                      </ListItemSecondaryAction>
-                    </ListItem>
-                 </Link>
-               })
-             }
-        </List>
-      </Paper>
-    )
+            <ListItem button>
+              <ListItemAvatar>
+                <Avatar>
+                  <Person />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={item.name} />
+              <ListItemText primary={"About: " + item.about} />
+              <ListItemText primary={"Administrator: " + item.admin} />
+              <DeleteUser userId={item._id} />
+              <ListItemSecondaryAction>
+                <IconButton>
+                  <ArrowForward />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          </Link>
+        })
+        }
+      </List>
+    </Paper>
+  )
 }
