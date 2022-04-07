738afe703e1c694a4f1ea7300e2075db5f5de9d7
bryce2302 <43371125+bryce2302@users.noreply.github.com>
2022-04-07 16:29:31 +0100

    removed import

--- server/express.js
+++ server/express.js
@@ -8,7 +8,7 @@ import helmet from 'helmet'
 import Template from './../template'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
-import commentsRoutes from './routes/comments.routes'
+//import commentsRoutes from './routes/comments.routes'
 // modules for server side rendering
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
