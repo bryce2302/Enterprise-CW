abbbbdc40a69e1e173386a98cb53d3b7557d6562
bryce2302 <43371125+bryce2302@users.noreply.github.com>
2022-04-08 16:07:21 +0100

    trying to delete

--- client/user/AdminDeleteUser.js
+++ client/user/AdminDeleteUser.js
@@ -24,13 +24,10 @@ export default function AdminDeleteUser(props) {
     remove({
       userId: props.userId
     }, {t: jwt.token}).then((data) => {
-      if (data && data.error) {
-        console.log(data.error)
-      } else {
-        auth.clearJWT(() => console.log('deleted'))
+      auth.clearJWT(() => console.log('deleted'))
         setRedirect(true)
       }
-    })
+    )
   }
   const handleRequestClose = () => {
     setOpen(false)
