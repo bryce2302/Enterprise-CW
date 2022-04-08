88be59eed2856e97d899b021206955e48790bfba
bryce2302 <43371125+bryce2302@users.noreply.github.com>
2022-04-08 12:09:35 +0100

    more chnages to route names

--- server/routes/comments.routes.js
+++ server/routes/comments.routes.js
@@ -9,13 +9,13 @@ router.route('/api/comments')
   .post(authCtrl.requireSignin, commentCtrl.create)
 
 
-router.route('/api/comments/commentsID/:commentsID')
+router.route('/api/commentsID/:commentsID')
   .get(authCtrl.requireSignin, commentCtrl.list)
 
 router.param('commentsID', commentCtrl.commentsByID)
 
 
-router.route('/api/comments/commentsByID_User/:commentsByID_User')
+router.route('/api/commentsByID_User/:commentsByID_User')
   .get(authCtrl.requireSignin, commentCtrl.list)
 
 router.param('commentsByID_User', commentCtrl.commentsByID_User)
