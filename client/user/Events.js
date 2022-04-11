c40df91e794af7e9f4dbc11821e6a924e7cdf7ce
bryce2302 <bryce_dawson@outlook.com>
2022-04-11 20:03:59 +0100

    remiving reload

--- client/user/Events.js
+++ client/user/Events.js
@@ -90,7 +90,7 @@ export default function Events({ match }) {
       } else {
        // setValues({ ...values, error: '', open: true})
       }
-      location.reload()
+      //location.reload()
     })
   }
 
