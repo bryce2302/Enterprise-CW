8616c21b1ce3b0871d8dfc593b31936b635e0271
bryce2302 <bryce_dawson@outlook.com>
2022-04-11 21:00:00 +0100

    checking event id

--- client/user/Events.js
+++ client/user/Events.js
@@ -84,7 +84,7 @@ export default function Events({ match }) {
 
  
   function removeEvent (event){
-    remove(event, {t: auth.isAuthenticated().token}, auth.isAuthenticated().user._id ).then((data) =>{
+    remove(event, {t: auth.isAuthenticated().token}, auth.isAuthenticated().event._id ).then((data) =>{
       if (data.error) {
        // setValues({ ...values, error: data.error})
       } else {
