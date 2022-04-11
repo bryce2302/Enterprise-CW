0f8f3af1e0fb6beac1cef84b1ed293181a9a2dcd
bryce2302 <bryce_dawson@outlook.com>
2022-04-11 11:45:41 +0100

    RESTING EVENTS

--- client/user/CommentsCreate.js
+++ client/user/CommentsCreate.js
@@ -69,7 +69,7 @@ export default function CommentsCreate() {
         setValues({ ...values, error: '', open: true})
       }
     })
-    location.reload();
+    //location.reload();
   }
 
     return (<div>
