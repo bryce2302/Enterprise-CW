62da260da2628915d2edb5ea5ecb785b80e62ba5
bryce2302 <bryce_dawson@outlook.com>
2022-04-11 21:58:07 +0100

    sizing the events

--- client/user/Comments.js
+++ client/user/Comments.js
@@ -84,7 +84,7 @@ export default function Comments() {
                     return (
                       <Card className = {classes.commentsStyle}>
 
-                                <ListItemText primary={item.comments}/>
+                                <ListItemText variant="h7" primary={item.comments}/>
                                
                                 <Button variant="contained" color="primary" size="medium" onClick={() => removeComment(item._id)}>Delete</Button>
                          
