e9660cc119e04a0ce7d013a1cbea1da57f037ca1
bryce2302 <bryce_dawson@outlook.com>
2022-04-11 22:50:55 +0100

    more coloiur changes

--- client/theme.js
+++ client/theme.js
@@ -8,7 +8,7 @@ const theme = createTheme({
     palette: {
       primary: {
       light: '#5c67a3',
-      main: '#42d494',
+      main: '#B2AC88',
       dark: '#2e355b',
       contrastText: '#fff',
     },
@@ -18,7 +18,7 @@ const theme = createTheme({
       dark: '#c60055',
       contrastText: '#000',
     },
-      openTitle: '#42d494',
+      openTitle: '#B2AC88',
       protectedTitle: pink['400'],
       type: 'light'
     }
