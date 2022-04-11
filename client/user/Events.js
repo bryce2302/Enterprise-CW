112d87f834c0dba46f1c6c0c279f321c1b4f85b0
bryce2302 <bryce_dawson@outlook.com>
2022-04-11 20:10:12 +0100

    removed depricated code

--- client/user/Events.js
+++ client/user/Events.js
@@ -14,17 +14,13 @@ import {update} from './api-events'
 import {remove} from './api-events'
 
 const useStyles = makeStyles(theme => ({
-  root: theme.mixins.gutters({
-    padding: theme.spacing(1),
-    margin: theme.spacing(5)
-  }),
   eventStyle:{
     margin: `5px 5px 12px 5px`,
     backgroundColor: `#f6dcf7`,
   },
 
   title: {
-    margin: `auto`,
+    margin: `${theme.spacing(4)}px 0 ${theme.spacing(2)}px`,
     color: theme.palette.openTitle
   }
 }))
