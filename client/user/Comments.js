d9192c56889b50e52450b239851a11e44d477c4d
bryce2302 <bryce_dawson@outlook.com>
2022-04-11 10:36:43 +0100

    re test

--- client/user/Comments.js
+++ client/user/Comments.js
@@ -55,9 +55,9 @@ export default function Comments() {
   function removeComment (comment){
     remove(comment, {t: auth.isAuthenticated().token}, auth.isAuthenticated().user._id).then((data) =>{
       if (data.error) {
-       //removed set values
+        setValues({ ...values, error: data.error})
       } else {
-        //removed set values
+        setValues({ ...values, error: '', open: true})
       }
       location.reload()
     })
