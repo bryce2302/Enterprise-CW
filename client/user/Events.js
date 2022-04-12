9c3d8397b8f004e2f5d67d6e21a2b7c0c307c727
bryce2302 <bryce_dawson@outlook.com>
2022-04-12 02:09:59 +0100

    added import

--- client/user/Events.js
+++ client/user/Events.js
@@ -12,6 +12,8 @@ import Button from '@material-ui/core/Button'
 import {list} from './api-events.js'
 import {update} from './api-events'
 import {remove} from './api-events'
+import {read} from './api-events'
+
 
 const useStyles = makeStyles(theme => ({
   root: theme.mixins.gutters({
