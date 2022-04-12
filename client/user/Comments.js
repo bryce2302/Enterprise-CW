19a7f9fea08fe5de7fac5cab6f1c2e2e5a9abd05
bryce2302 <bryce_dawson@outlook.com>
2022-04-12 11:41:24 +0100

    fixed wrong imports

--- client/user/Comments.js
+++ client/user/Comments.js
@@ -5,11 +5,11 @@ import Typography from '@material-ui/core/Typography'
 import List from '@material-ui/core/List'
 import ListItemText from '@material-ui/core/ListItemText'
 import auth from '../auth/auth-helper'
-import {list} from './api-comments.js.js'
+import {list} from './api-comments..js'
 import { CardContent } from '@material-ui/core'
 import Card from '@material-ui/core/Card'
 import Button from '@material-ui/core/Button'
-import {remove} from './api-comments.js.js'
+import {remove} from './api-comments..js'
 
 
 
